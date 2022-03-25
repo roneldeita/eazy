7c1ab0de968b1bca0b4468978fc428632fc8e6fe
Ronel Deita <ardy@Ronels-MacBook-Pro.local>
2022-03-25 17:27:32 +0800

    updated install

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,47 @@ import RoutesTree from './components/RoutesTree';
 const App = () => {
   
   useEffect(()=>{
-    let defferedPrompt:any;
-    const addbtn:any = document.querySelector('.btn');
+    let deferredPrompt:any;
+    const buttonInstall:any = document.querySelector('.btn');
     
-    window.addEventListener('beforeinstallprompt', event => {
-        event.preventDefault();
-        defferedPrompt = event
-        addbtn.style.display = 'block';
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      buttonInstall.style.display = 'block';
+      //showInstallPromotion();
+      // Optionally, send analytics event that PWA install promo was shown.
+      console.log(`'beforeinstallprompt' event was fired.`);
     });
     
-    addbtn.addEventListener('click', (event:any) => {
-        defferedPrompt.prompt();
-    
-        defferedPrompt.userChoice.then((choice:any) => {
-            if(choice.outcome === 'accepted'){
-                console.log('user accepted the prompt')
-            }
-            defferedPrompt = null;
-        })
-    })  
+    buttonInstall.addEventListener('click', async () => {
+      // Hide the app provided install promotion
+     // hideInstallPromotion();
+     buttonInstall.style.display = 'none';
+      // Show the install prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      const { outcome } = await deferredPrompt.userChoice;
+
+
+      // Optionally, send analytics event with outcome of user choice
+      console.log(`User response to the install prompt: ${outcome}`);
+   
+      // We've used the prompt, and can't use it again, throw it away
+      deferredPrompt = null;
+    });
+
+    window.addEventListener('appinstalled', () => {
+      // Hide the app-provided install promotion
+      //hideInstallPromotion();
+      buttonInstall.style.display = 'none';
+      // Clear the deferredPrompt so it can be garbage collected
+      deferredPrompt = null;
+      // Optionally, send analytics event to indicate successful install
+      console.log('PWA was installed');
+    });
   })
   return (
     <Layout>
