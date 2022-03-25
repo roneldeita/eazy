9cfe2f608c7ddb0d3d567c326841b2391e95dde0
Ronel Deita <ardy@Ronels-MacBook-Pro.local>
2022-03-25 16:21:10 +0800

    added install button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from './components/Layout';
 import RoutesTree from './components/RoutesTree';
-import { InstallPWA } from './components/install/installPwa';
 
-const App = () =>{
+const App = () => {
+  
+  useEffect(()=>{
+    let defferedPrompt:any;
+    const addbtn:any = document.querySelector('.btn');
+    
+    window.addEventListener('beforeinstallprompt', event => {
+        event.preventDefault();
+        defferedPrompt = event
+        addbtn.style.display = 'block';
+    });
+    
+    addbtn.addEventListener('click', (event:any) => {
+        defferedPrompt.prompt();
+    
+        defferedPrompt.userChoice.then((choice:any) => {
+            if(choice.outcome === 'accepted'){
+                console.log('user accepted the prompt')
+            }
+            defferedPrompt = null;
+        })
+    })  
+  })
   return (
     <Layout>
       <RoutesTree/>
+      <button className='btn' style={{display:'none'}}>Install Eazy</button>
     </Layout>
   );
 }
