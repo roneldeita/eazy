677b05a4ddd481643ce518541cf1480ef006a516
roneldeita <ronel.deita@beehive.hk>
2022-04-12 16:04:06 +0800

    fixed route name error

--- src/components/RoutesTree.tsx
+++ src/components/RoutesTree.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom"
 import Home from "../Pages/Home";
-import Login from "../Pages/Login";
+import Signup from "../Pages/Signup";
 
 const RoutesTree = () =>{
   return(
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   )
 }
